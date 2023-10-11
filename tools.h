051ea88ff83043fcdfee1e9c133baa27a615cf12
johann <johann-l.schulz@outlook.com>
2023-10-11 15:03:22 +0200

    #1 added docstrings and escapesequenzen.h

--- tools.h
+++ tools.h
@@ -1,9 +1,36 @@
 #ifndef tools_h
-#define tools_h
+    #define tools_h
 
-void clearBuffer(void);
-void waitForEnter(void);
-void clearScreen(void);
-int askYesOrNo(char *);
+    /***************************************************************************
+    *  clearBuffer
+    *  Loescht den Tastaturpuffer
+    *  Parameter: -
+    *  Ergebnis : -
+    ***************************************************************************/
+    void clearBuffer(void);
+
+    /***************************************************************************
+    *  waitForEnter
+    *  wartet auf Enter oder so
+    *  Parameter: -
+    *  Ergebnis : -
+    ***************************************************************************/
+    void waitForEnter(void);
+
+    /***************************************************************************
+    *  clearScreen
+    *  Loescht alle sichtbare Ausgabe im Konsolen-Fenster
+    *  Parameter: -
+    *  Ergebnis : -
+    ***************************************************************************/
+    void clearScreen(void);
+
+    /***************************************************************************
+    *  askYesOrNo
+    *  Fragt ab, ob yes oder no, validiert Benutzereingabe
+    *  Parameter: *Question: Text, der gefragt werden soll
+    *  Ergebnis : Gibt ja: 1 oder nein: 0 zurueck
+    ***************************************************************************/
+    int askYesOrNo(char *);
 
 #endif /* tools_h */
