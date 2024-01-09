0ca8df1a5a39b6d9157345005ce34280e8d4c3da
julian <julian.barnowski2004@gmail.com>
2024-01-09 23:38:37 +0100

    #6 added *removeListElement

--- list.c
+++ list.c
@@ -35,6 +35,35 @@ void insertInDList(sAppointment *New, int (*cmpfct) (sAppointment *, sAppointmen
 }
 
 // Funktion zum Entfernen eines Termins aus der doppelt verketteten Liste
-void removeFromDList(sAppointment *pAppointment, sAppointment **pFirst, sAppointment **pLast) {
-    // Implementierung des Entfernens
+sAppointment *removeListElement(sAppointment *Remove, int (*cmpfct)(sAppointment *, sAppointment *)) {
+    sAppointment *tmp = First, *rmv = NULL;
+
+    if (Remove) {
+        if (First == NULL) {
+            ; // Fall 1: Liste ist leer
+        } else if ((*cmpfct)(First, Remove) == 0) { // Fall 2: erstes Element entfernen
+            rmv = First;
+            First = First->Next;
+            if (First == NULL) {
+                Last = NULL; // Fall 2: Sonderfall erstes Element == letztes Element
+            } else {
+                First->Prev = NULL;
+            }
+        } else if ((*cmpfct)(Last, Remove) == 0) { // Fall 3: letztes Element entfernen
+            rmv = Last;
+            Last = Last->Prev;
+            Last->Next = NULL;
+        } else {
+            while (tmp->Next) { // Fall 4: sonstiges Element entfernen
+                if ((*cmpfct)(tmp->Next, Remove) == 0) {
+                    rmv = tmp->Next;
+                    tmp->Next = rmv->Next;
+                    tmp->Next->Prev = tmp;
+                    break;
+                }
+                tmp = tmp->Next;
+            }
+        }
+    }
+    return rmv;
 }
